14b8c57abc919fbd5bed325aba460e18c42da0fe
Tarciana Oliveira <anarumiroan@gmail.com>
2023-07-26 22:43:59 -0300

    iniciando o javascritp

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+
+const sons = {
+    'A': 'boom.wav',
+    'S': 'clap.wav',
+    'D': 'hihat.wav',
+    'F': 'kick.wav',
+    'G': 'openhat.wav',
+    'H': 'ride.wav',
+    'J': 'snare.wav',
+    'K': 'tink.wav',
+    'L': 'tom.wav'
+}
+
+const criarDiv = (texto) => {
+    const div = document.createElement('div');
+    div.classList.add('key');
+    div.textContent = texto;
+    div.id = texto;
+    document.getElementById('container').appendChild(div);
+}
+
+criarDiv();
+
+const exibirSons = (sons) => Object.keys(sons).forEach(criarDiv);
+
+
+exibirSons(sons);
