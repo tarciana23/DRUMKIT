32534c2ef448d63fb6e748679103c2387e5e7679
Tarciana Oliveira <anarumiroan@gmail.com>
2023-07-31 21:21:54 -0300

    tentando carregar audios no javascript

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 'use strict';
-
+const audios = [
+    document.getElementById('audio1'),
+    document.getElementById('audio2'),
+    document.getElementById('audio3'),
+    document.getElementById('audio4'),
+    document.getElementById('audio5'),
+    document.getElementById('audio16'),
+    document.getElementById('audio7'),
+    document.getElementById('audio8'),
+    document.getElementById('audio9'),
+]
 
 const sons = {
     'A': 'boom.wav',
@@ -26,4 +36,18 @@ criarDiv();
 const exibirSons = (sons) => Object.keys(sons).forEach(criarDiv);
 
 
+const tocarSom = () => {
+    console.log("oi")
+    audios[0].play();
+}
+
+
+
+function ativarDiv(evento) {
+    const letra = evento.target.i;
+    tocarSom(letra);
+
+
+}
 exibirSons(sons);
+document.getElementById('container').addEventListener('click', tocarSom);
